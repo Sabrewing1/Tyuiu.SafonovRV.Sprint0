7dc3ca12a8d61251c9dcfaaeaa9284afaad660e6
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-09-13 20:40:35 +0500

    Task3

---
+++ Tyuiu.SafonovRV.Sprint0.Task3.V0.Test/DataService.cs
@@ -0,0 +1,21 @@
+using Tyuiu.SafonovRV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.SafonovRV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        private static int Sum(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+      
